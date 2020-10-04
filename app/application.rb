7922659ee24d4415d3bec80c2aebb9671a71d0e7
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-04 19:37:09 -0400

    finished lab

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,10 @@ class Application
 
     def call(env)
         resp = Rack::Response.new
-        time = Time.new
-        if time.hour >= 12
+        if Time.now.hour < 12
             resp.write "Good Morning!"
         else
-            resp.write "Good Afternoon!"
+            resp.write "Good Afternoon"
         end
         resp.finish
     end 
